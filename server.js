4031e93ddda829c22d3524fb477b2a32c31b6d04
Okhitoya Alex <lexmanthefirst@gmail.com>
2025-03-18 17:40:47 -0400

    Integrated dynamic car inventory, error handling and robust error handling

--- server.js
+++ server.js
@@ -5,29 +5,54 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express");
-const env = require("dotenv").config();
+const express = require('express');
+const env = require('dotenv').config();
 const app = express();
-const static = require("./routes/static");
-const expressLayouts = require("express-ejs-layouts");
+const static = require('./routes/static');
+const expressLayouts = require('express-ejs-layouts');
+const baseController = require('./controllers/baseController');
+const inventoryRoute = require('./routes/inventoryRoute');
+const utilities = require('./utilities/');
 
 /* ***********************
  * Routes
  *************************/
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 app.use(expressLayouts);
-app.set("layout", "./layouts/layout"); // n
-
+app.set('layout', './layouts/layout'); // n
 app.use(static);
 
 /* ***********************
  * Homepage Route
  * Renders the "index" view with a title
  *************************/
-app.get("/", function (req, res) {
-  res.render("index", { title: "Home" });
+
+app.get('/', utilities.handleErrors(baseController.buildHome));
+app.use('/inv', inventoryRoute);
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({ status: 404, message: 'Sorry, we appear to have lost that page.' });
 });
 
+/* ***********************
+ * Express Error Handler
+ * Place after all other middleware
+ *************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav();
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  if (err.status == 404) {
+    message = err.message;
+  } else {
+    message = 'Oh no! There was a crash. Maybe try a different route?';
+  }
+  res.render('errors/error', {
+    title: err.status || 'Server Error',
+    message,
+    nav,
+  });
+});
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
---
+++ utilities/index.js
@@ -0,0 +1,73 @@
+const invModel = require('../models/inventory-model');
+const Util = {};
+
+/* ************************
+ * Constructs the nav HTML unordered list using template literals
+ ************************** */
+
+Util.getNav = async function (req, res, next) {
+  let data = await invModel.getClassifications();
+  let list = `<ul class = "menuLinks">`;
+  list += `<li><a href="/" title="Home page">Home</a></li>`;
+
+  data.rows.forEach(row => {
+    list += `
+      <li>
+        <a href="/inv/type/${row.classification_id}" title="See our inventory of ${row.classification_name} vehicles">
+          ${row.classification_name}
+        </a>
+      </li>`;
+  });
+
+  list += `</ul>`;
+  return list;
+};
+module.exports = Util;
+
+Util.buildClassificationGrid = async function (data) {
+  let grid = '';
+
+  if (data.length > 0) {
+    grid = `<ul id="inv-display">`;
+
+    data.forEach(vehicle => {
+      grid += `
+        <li>
+          <a href="../../inv/detail/${vehicle.inv_id}" title="View ${
+        vehicle.inv_make
+      } ${vehicle.inv_model} details">
+            <img src="${vehicle.inv_thumbnail}" alt="Image of ${
+        vehicle.inv_make
+      } ${vehicle.inv_model} on CSE Motors" />
+          </a>
+          <div class="namePrice">
+            <hr />
+            <h2>
+              <a href="../../inv/detail/${vehicle.inv_id}" title="View ${
+        vehicle.inv_make
+      } ${vehicle.inv_model} details">
+                ${vehicle.inv_make} ${vehicle.inv_model}
+              </a>
+            </h2>
+            <span>$${new Intl.NumberFormat('en-US').format(
+              vehicle.inv_price
+            )}</span>
+          </div>
+        </li>`;
+    });
+
+    grid += `</ul>`;
+  } else {
+    grid = `<p class="notice">Sorry, no matching vehicles could be found.</p>`;
+  }
+
+  return grid;
+};
+
+/* ****************************************
+ * Middleware For Handling Errors
+ * Wrap other function in this for
+ * General Error Handling
+ **************************************** */
+Util.handleErrors = fn => (req, res, next) =>
+  Promise.resolve(fn(req, res, next)).catch(next);
